cc233a533f5d4d8da966ab887c996b3059b47b08
vikramjit-sidhu <vikram.sidhu.007@gmail.com>
2015-01-30 00:33:24 +0530

    started doc dist code, finish asap

---
+++ mit_intro_algos/doc_dist.py
@@ -0,0 +1,19 @@
+"""
+Document distance, my implementation
+"""
+import sys
+
+
+
+def main():
+    if not sys.argv[1:]:
+        print("well this is boring, over and out")
+        return
+    filename1 = sys.argv[1]
+    filename2 = sys.argv[2]
+    doc
+
+
+
+if __name__ == '__main__':
+    main()
