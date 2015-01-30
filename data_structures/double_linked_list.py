27fb8e361a9952a8f04008081d3431f3ab0642bf
vikramjit-sidhu <vikram.sidhu.007@gmail.com>
2015-01-30 15:01:33 +0530

    finished double_linked_list implementation, need to compare it now with list, use timeit module. minor changes to doc_dist

--- data_structures/double_linked_list.py
+++ data_structures/double_linked_list.py
@@ -7,18 +7,18 @@ class LinkedList:
     """
     Class for creating a double linked list.
     """
-    _head = None
-    _tail = None
+    head = None
+    tail = None
     def __init__(self, key):
-        if not self._head:
-            self._head = self
+        if not self.__class__.head:
+            self.__class__.head = self
         self.key = key
         self.next = None
         self.previous = None
     
     
     def __iter__(self):
-        self.current_node = self._head
+        self.current_node = self.__class__.head
         return self
         
         
@@ -32,39 +32,75 @@ class LinkedList:
     
     
     def __contains__(self, key):
-        for node in self.__iter__():
-            print(node)
+        for node_key in self.__iter__():
+            if key == node_key:
+                return True
+        return False
     
-    # def set_head(self, node):
-        # """
-        # Set root of linked list
-        # """
-        # self._head = node
-        # return self._head
+        
+    def add(self, key):
+        """
+        Given a key, makes a node and appends it to tail of linked list.
+        Updates tail pointer accordingly
+        """
+        if not self.__class__.tail:
+            self.__class__.tail = self.__class__.head
+        node = LinkedList(key)
+        prev_tail = self.__class__.tail
+        prev_tail.next = node
+        node.previous = prev_tail
+        self.__class__.tail = node
+        return self.__class__.head
         
         
-    def add_node(self, node):
+    def remove(self, key):
         """
-        Adds a node to end of linked list, updates the tail pointer
+        Given a key, searches for it and removes it from linked list.
+        Returns the value of key
         """
-        if not self._tail:
-            self._tail = self._head
-        self._tail.next = node
-        node.previous = self._tail
-        self._tail = node
-        return self._head
+        curr_node = self.__class__.head
+        found = False
+        while curr_node:
+            if curr_node.key == key:
+                found = True
+                break
+            curr_node = curr_node.next
+        if not found:
+            print("Key {0} not found, hence not deleted".format(key))
+            return None
+        curr_node.previous.next = curr_node.next
+        curr_node.next.previous = curr_node.previous
+        return key
+    
         
 
         
 def main():
-    bag_num = list(range(500))
+    bag_num = list(range(1000))
     foo = random.choice(bag_num)
     bag_num.remove(foo)
+    link_list = LinkedList(foo)
+
+    while bag_num:
+        foo = random.choice(bag_num)
+        bag_num.remove(foo)
+        link_list.add(foo)
+        
     
-    root = LinkedList(foo)
-    root.set_head(root)
-    
+    # ll = LinkedList(0)
+    # for i in range(10, 210, 10):
+        # ll.add(i)
+        
+    # for i in ll:
+        # print(i)
+    # ll.remove(20)
+    # ll.remove(50)
+    # ll.remove(670)
+    # ll.remove(110)
 	
+    # for i in ll:
+        # print(i)    
 	
 if __name__ == '__main__':
     main()
+    
--- mit_intro_algos/doc_dist.py
+++ mit_intro_algos/doc_dist.py
@@ -4,6 +4,30 @@ Document distance, my implementation
 import sys
 
 
+def isalphanum(word):
+    """
+    Checks if word is alphanumeric
+    """
+    
+
+
+def get_word_list(filename):
+    """
+    Given filename, creates its wordlist.
+    """
+    while open(filename) as file:
+        for line in file:
+            
+
+
+def create_doc_vector(filename):
+    """
+    Given filename, create a dict mapping the words in file, to their 
+    frequency of occurance.
+    First obtains a list of words calling get_word_list method
+    """
+    word_list = get_word_list(filename)
+
 
 def main():
     if not sys.argv[1:]:
@@ -11,7 +35,8 @@ def main():
         return
     filename1 = sys.argv[1]
     filename2 = sys.argv[2]
-    doc
+    doc_vector1 = create_doc_vector(filename1)
+    doc_vector2 = create_doc_vector(filename2)
 
 
 
