f9be30bb2c6f231a6a21934f86ab5309f59c8ca3
vikramjit-sidhu <vikram.sidhu.007@gmail.com>
2015-02-11 16:51:48 +0530

    finished fibonacci heap, update_key not working properly as bfs is not searching key properly

--- data_structures/fib_heap.py
+++ data_structures/fib_heap.py
@@ -23,6 +23,23 @@ class FibHeapNode:
         node.parent = self
         self.children.append(node)
         self.degree += node.degree + 1
+    
+    def remove_child(self, child_rem):
+        for index, child in enumerate(self.children):
+            if child is child_rem:
+                self.degree -= child.degree + 1
+                del self.children[index]
+                break
+        else:
+            print("\nChild with key {0} not found in node with key: {1}".format(child_rem.key, self.key))
+            
+    def traversal(self):
+        print(self.key, end=' ->')
+        for child in self.children:
+            print(child.key, end=' ')
+        print('\n', end=' ')
+        for child in self.children:
+            child.traversal()
         
     
 class FibonacciHeap:
@@ -30,32 +47,44 @@ class FibonacciHeap:
         self.roots_list = []
         self.min_root_index = None
         
-    def insert(self, key):
-        node = FibHeapNode(key)
+    def insert(self, elt):
+        """ Simply append node to roots list, leave maintenance operations for later """
+        if isinstance(elt, FibHeapNode):
+            node = elt
+        else:
+            elt_key = elt
+            node = FibHeapNode(elt)
         if self.min_root_index is None:
             self.min_root_index = 0
         else:
             min_node = self.roots_list[self.min_root_index]
-            if key < min_node.key:
+            if node.key < min_node.key:
                 self.min_root_index = len(self.roots_list)
         self.roots_list.append(node)
-        
+    
+    def _add_as_root(self, node):
+        """ Add a node as root, change its parent property, mark it as False, etc """
+        node.parent = None
+        node.marked = False
+        self.roots_list.append(node)
+    
     def extract_min(self):
+        """ Removes min node, makes its children as roots and starts consolidate operation """
         if self.min_root_index is None:
             return None
         min_node = self.roots_list[self.min_root_index]
         min_key = min_node.key
         del self.roots_list[self.min_root_index]
         for children in min_node.children:
-            self.roots_list.append(children)
-        del(min_node)
+            self._add_as_root(children)
+        # del(min_node) #operation not really necessary
         self._consolidate()
         self._set_min_index()
         return min_key
     
     def _consolidate(self):
-        """ Merge all roots which have the same degree. """
-        node_degree_hash = {}   #hash containing node degree:node index pairs
+        """ Merge all roots which have the same degree. (same number of children) """
+        node_degree_hash = {}   #hash containing, node degree:node index pairs
         for node in self.roots_list:
             if node.degree in node_degree_hash:
                 while node.degree in node_degree_hash:
@@ -86,10 +115,57 @@ class FibonacciHeap:
                 min_index = index
         self.min_root_index = min_index
         
-    def update_key(self, new_key):
+    def update_key(self, old_key, new_key):
+        """ 
+        Search node with BFS and update key 
+        IMPORTANT: only supports decrease of key, increase not supported
+        """
+        if old_key < new_key:
+            print("New key is greater, change not possible")
+            return None
+        from queue import Queue
+        qu = Queue()
+        for root in self.roots_list:
+            if root.key <= old_key:
+                qu.put_nowait(root)
+        while not qu.empty():
+            node = qu.get_nowait()
+            if node.key == old_key:
+                node.key = new_key
+                self._maintain_heap(node)
+                break
+            #accessing children of node to put into queue
+            for child in node.children:
+                if child.key <= old_key:
+                    qu.put_nowait(child)
+        else:
+            #key not found, hence else is executed
+            print("\nKey: {0} not found in heap, hence not updated".format(old_key))
+        del(qu)
         
+    def _maintain_heap(self, node):
+        """ 
+        Checks if node.key < parent.key if so branches it off and makes it a separate root, marking its parent (done in this method)
+        If parent was already marked, the parent has to be made a separate root as well, 
+        continuing until an unmarked node is found or root (done in _check_marking)
+        """
+        if node.parent and node.key < node.parent.key:
+            father = node.parent
+            father.remove_child(node)
+            self.insert(node)
+            self._check_marking(father)
+            
+    def _check_marking(self, node):
+        """ If node has marked property set, remove it from its parent and make it a root, else mark it """
+        if node.marked:
+            self._add_as_root(node)
+            if node.parent:
+                self._check_marking(node.parent)
+        else:
+            node.marked = True
+            
     
-
+    
 def heap_node_test():
     node_main = FibHeapNode(8)
     for i in range(10):
