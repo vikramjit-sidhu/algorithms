2a00a2f126267f65bd4be4282cd7fdc389deaa15
vikramjit-sidhu <vikram.sidhu.007@gmail.com>
2015-02-17 16:45:49 +0530

    some more hacker rank q's

---
+++ hacker_rank/max_xor.py
@@ -0,0 +1,14 @@
+"""
+Hacker Rank
+maximising XOR
+https://www.hackerrank.com/challenges/maximizing-xor
+"""
+
+
+
+def main():
+    l = int(input().strip())
+    r = int(input().strip())
+    
+if __name__ == '__main__':
+    main()
--- hacker_rank/utopian_tree.py
+++ hacker_rank/utopian_tree.py
@@ -6,14 +6,34 @@ https://www.hackerrank.com/challenges/utopian-tree
 from math import pow
 
 def find_growth(seasons):
-    if seasons%2 == 0:
-        
+    """
+    Using pattern: 
+    after season 0 ht = 2^1-1
+    after season 1 ht = 2^2-1-1
+    after season 2 ht = 2^2-1
+    after season 3 ht = 2^2-1-1
+    after season 4 ht = 2^3-1
+    after season 5 ht = 2^4-1-1
+    after season 6 ht = 2^4-1 and so on
+    Hence this forms two AP's:
+    one : 0, 2, 4, 6, 8, 10, 12,.... (corresponding to the even seasons)
+    and : 1, 2, 3, 4, 5, 6, 7, .....  (corresponding to exponent of 2 in even seasons
+    since we know the season, we have to find the corresponding term in 2nd AP
+    """
+    if seasons%2 != 0:
+        seasons += 1
+        ht_appendage = -1
+    else:
+        ht_appendage = 0
+    n = int(seasons/2) + 1 #this is exponent of 2 needed
+    ht_season = int(pow(2, n)) - 1 + ht_appendage
+    return ht_season
+
 
 def main():
     test_cases = int(input().strip())
     for i in range(test_cases):
         print(find_growth(int(input().strip())))
-    
     
 if __name__ == '__main__':
     main()
