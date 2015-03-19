aeb1d38636c99d09a3de30d39a063bac9f42aff1
vikramjit-sidhu <vikram.sidhu.007@gmail.com>
2015-03-19 13:38:23 +0530

    finished another hacker rank q'n and code jam practice

--- code_jam/store_credit.py
+++ code_jam/store_credit.py
@@ -2,19 +2,32 @@
 Google code jam question - Practice
 https://code.google.com/codejam/contest/351101/dashboard#s=p0
 """
-
-def binary_search(list, item):
-    """ Returns True if item found in list """
-    mid = len(list) // 2
-    if item == list[mid]:
+import pdb
+def binary_search(list, start, end, item):
+    if start > end:
+        return False
+    mid = start + (end-start)//2
+    if (item == list[mid]):
         return True
     elif list[mid] > item:
-        
-
-def find_items(store_credit, store_items):
+        return binary_search(list, start, mid-1, item)
+    else:
+        return binary_search(list, mid+1, end, item)
+            
+def find_items(store_credit, store_items, num_store_items):
     """ Running time is nlog(n), sorting items first and then using binary search """
-    sorted_items = sorted(store_items)
-
+    sorted_store_items = sorted(store_items)
+    for index, item  in enumerate(store_items):
+        item_tofind = store_credit - item
+        if binary_search(sorted_store_items, 0, num_store_items-1, item_tofind):
+            item_tofind_index = store_items.index(item_tofind)
+            if item_tofind_index == index:
+                try:
+                    item_tofind_index = index + 1 + store_items[index+1:].index(item_tofind)
+                except Exception:
+                    continue
+            return (index, item_tofind_index)
+    return (-1, -1) #should not execute
 
 def main():
     num_testcases = int(input().strip())
@@ -22,8 +35,8 @@ def main():
         store_credit = int(input().strip())
         num_items = int(input().strip())    #the number of items in store, 2 of these are selected
         store_items = [int(i) for i in input().strip().split(' ')]
-        item1, item2 = find_items(store_credit, store_items)
-        print("Case #{0}: {1} {2}".format(i+1, item1, item2))
+        item1, item2 = find_items(store_credit, store_items, num_items)
+        print("Case #{0}: {1} {2}".format(i+1, item1+1, item2+1))
 
 if __name__ == '__main__':
     main()
--- hacker_rank/sherlock_squares.py
+++ hacker_rank/sherlock_squares.py
@@ -5,35 +5,16 @@ https://www.hackerrank.com/challenges/sherlock-and-squares
 Find perfect squares in [a, b]
 """
 
-from math import sqrt
-
-number_status = {}  #hash containing the number and the boolean value as to whether it is a square
-
-def check_square(n):
-    """
-    Check if n is a perfect square, done by two ways
-    (using method described in wiki:
-    http://en.wikipedia.org/wiki/Square_number
-    1. See last digit of n it should be in [1, 4, 6, 9]
-    2. The last two digits should be in [00, 25]
-    """
-    if n in number_status:
-        return number_status[n]
-    last_digit = n % 10
-    last_two_digits = n % 100
-    if (last_digit in (1, 4, 6, 9)) or (last_two_digits in (0, 25)):
-        root = int(sqrt(n) + 0.5)
-        if pow(root, 2) == n:
-            number_status[n] = True
-            return True
-    number_status[n] = False
-    return False
+from math import sqrt, ceil, floor
 
 def count_squares(a, b):
-    count_sq = 0
-    for num in range(a, b+1):
-        if check_square(num):
-            count_sq += 1
+    """ 
+    Using logic, that number of integers in range ceiling(sqrt(a)) and floor(sqrt(b))
+    is the number of integers that have a perfect square
+    """
+    lower_lt = ceil(sqrt(a))
+    upper_lt = floor(sqrt(b))
+    count_sq = upper_lt - lower_lt + 1
     return count_sq
     
 def main():
