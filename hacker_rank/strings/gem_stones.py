56b5457b771dad696d0f2e1b43f4e88edc2d4820
vikramjit-sidhu <vikram.sidhu.007@gmail.com>
2015-03-24 11:56:31 +0530

    started hacker rank strings questions, finished one

---
+++ hacker_rank/strings/gem_stones.py
@@ -0,0 +1,26 @@
+"""
+Hacker Rank - Service Lane
+https://www.hackerrank.com/challenges/service-lane
+"""
+
+def main():
+    n = int(input().strip())
+    char_count = {} #hash containing the letters and the count of the number of strings they appear in
+    for i in range(n):
+        word = input().strip()
+        word_char_cnt = {}
+        for char in word:
+            if char not in word_char_cnt:
+                word_char_cnt[char] = True
+                if char in char_count:
+                    char_count[char] += 1
+                else:
+                    char_count[char] = 1
+    count_tot = 0
+    for key, val in char_count.items():
+        if val == n:
+            count_tot += 1
+    print(count_tot)
+
+if __name__ == '__main__':
+	main()
