87aac0aee9e688ac08b94ccdf0657c6678bcd05b
vikramjit-sidhu <vikram.sidhu.007@gmail.com>
2015-03-27 16:44:07 +0530

    finished bigger is greater strings question

--- hacker_rank/strings/bigger_is_greater.py
+++ hacker_rank/strings/bigger_is_greater.py
@@ -2,59 +2,39 @@
 Hacker Rank - Bigger is greater
 https://www.hackerrank.com/challenges/bigger-is-greater
 """
-import pdb
-def find_char_freq(word):
-    """ given a word, return hash containing its char, freq """
-    char_freq = {}
-    for char in word:
-        if char in char_freq:
-            char_freq[char] += 1
-        else:
-            char_freq[char] = 1
-    return char_freq
-
-def find_lesser_char(char, char_freq):
-    """ Find if there is a character lexicographically lesser to char in char_freq hash """
-    for i in range(ord(char), ord('a')-1, -1):
-        c1 = chr(i)
-        if c1 in char_freq and char_freq[c1] > 0:
-            return True
-    return False
     
-def next_greatest_char(char, char_freq):
-    """ Find the next greatest lexicographic character to char in char_freq """
-    for j in range(ord(char)+1, ord('z')+1):
-        if chr(j) in char_freq:
-            return chr(j)
-    return False
+def find_succ(char, char_freq):
+    """ Find next lexicographical character to char """
+    for i in range(ord(char)+1, 123):
+        c1 = chr(i)
+        if c1 in char_freq:
+            return (True, c1)
+    return (False, None)
     
 def next_greatest_word(word):
     """ find lexicographically next greatest word to 'word' by rearranging its characters """
-    pdb.set_trace()
-    char_freq = find_char_freq(word)
-    next_best = ''
-    for char in word:
-        char_freq[char] -= 1
-        if find_lesser_char(char, char_freq):
-            next_best += char
+    new_word = ''
+    char_freq = {word[-1]: 1}
+    start_char = len(word) - 2  #starting from 2nd last character, as last char has already been processed
+    for index in range(start_char, -1, -1):
+        c1 = word[index]
+        if c1 in char_freq:
+            char_freq[c1] += 1
         else:
-            #finding the next greatest character lexicographically to char and appending it to string
-            next_char = next_greatest_char(char, char_freq)
-            if next_char is not False:
-                char_freq[next_char] -= 1
-                next_best += next_char + char
-            else:
-                #if we could not find any character greater than char, no solution possible
-                return 'no answer'
+            char_freq[c1] = 1
+        succ = find_succ(c1, char_freq)[1]
+        if succ is not None:
+            char_freq[succ] -= 1
+            new_word = word[:index] + succ
             break
-    #appending the reamaining characters to next_best in lexicographica order (starting from the num of 'a' left, then num of 'b' left, etc)
-    for i in range(ord('a'), ord('z') + 1):
-        c3 = chr(i)
-        if c3 in char_freq and char_freq[c3] > 0:
-            next_best += c3*char_freq[c3]
-            char_freq[c3] = 0
-    return next_best
-
+    else:
+        return 'no answer'
+    from string import ascii_lowercase
+    for c2 in ascii_lowercase:
+        if c2 in char_freq and char_freq[c2] > 0:
+            new_word += c2*char_freq[c2]
+    return new_word
+    
 def main():
     num_cases = int(input().strip())
     for i in range(num_cases):
