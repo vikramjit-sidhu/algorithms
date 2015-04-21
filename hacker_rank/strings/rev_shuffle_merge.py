2cbaa4a9905099d66bd1896b0b650595e0514b3d
vikramjit-sidhu <vikram.sidhu.007@gmail.com>
2015-04-21 09:29:05 +0530

    removing trie implementation in rev_shuffle_merge

--- hacker_rank/strings/rev_shuffle_merge.py
+++ hacker_rank/strings/rev_shuffle_merge.py
@@ -3,12 +3,11 @@ Hacker Rank - Reverse Shuffle Merge
 https://www.hackerrank.com/challenges/reverse-shuffle-merge
 """
 
-import copy
+import copy, string
 
 class TrieNode:
     def __init__(self):
         self.word = ''
-        self.leaf = False
         self.char_freq = {} #the char remaining for node to create
         self.children = {}
 
@@ -32,8 +31,28 @@ def insert_in_trie(char, root):
                 new_node = TrieNode()
                 new_node.word = node.word + char
                 new_node.char_freq = copy.deepcopy(node.char_freq)
+                new_node.char_freq[char] -= 1
+                node.children[char] = new_node
         curr_level, next_level = next_level, []
     return root
+    
+def find_largest(node, len_word):
+    """
+    Find the largest word in the trie, by always taking the lexicographically largest char possible.
+    A lexicographically largest word of length 'len_word' is required.
+    node should be the root initially
+    """
+    rev_alphabet = list(string.ascii_lowercase)
+    rev_alphabet.reverse()
+    rev_str = ''
+    while len_word > 0:
+        for char in rev_alphabet:
+            if char in node.children:
+                node = node.children[char]
+                len_word -= 1
+                rev_str += char
+                break
+    return rev_str
 
 def build_trie(char_freq, word):
     root = TrieNode()
@@ -48,6 +67,12 @@ def find_base_str(string):
     char_freq = get_char_freq(string)
     char_freq = {key:(val//2) for key, val in char_freq.items()}
     root = build_trie(char_freq, string)
+    len_word = sum(char_freq.values())
+    #the substring input contains the merge of reversed string and the spliced string
+    #hence since we need to find the lexicographically minimal string, we find lexicographically maximum reversed string
+    #the lexicographically maximum reversed string is found by a subsequence of the string input (merged string)
+    rev_str = find_largest(root, len_word)
+    return (rev_str[::-1])
 
 def main():
     string = input().strip()
