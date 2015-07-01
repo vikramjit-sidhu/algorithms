47d3645d5650d388ac6153d9108ec1e0efcbebbb
vikramjit-sidhu <vikram.sidhu.007@gmail.com>
2015-07-01 15:14:37 +0530

    trying something new in connected cell grid

--- hacker_rank/search/connected_cell_grid.py
+++ hacker_rank/search/connected_cell_grid.py
@@ -2,47 +2,73 @@
 Hacker Rank - Connected Cells
 https://www.hackerrank.com/challenges/connected-cell-in-a-grid
 """
+import pdb
 
 def find_connections(rows, cols, matrix):
-    graph = create_graph(rows, cols, matrix)
-    print(graph)
+    graph, nodes_tovisit = create_graph(rows, cols, matrix)
+    nodes_visited = {}
+    for nd,it in graph.items():
+        print('{0}: {1}'.format(nd, it))
+    longest_len = 0
+    for row, col in nodes_tovisit:
+        if (row,col) not in nodes_visited:
+            nodes_visited[(row,col)] = 1
+        else:   #node has already been visited
+            continue
+        curr_len = 1
+        curr_nodes = [] #nodes to visit in the current context
+        curr_nodes.extend(graph[(row,col)])
+        for i,j in curr_nodes:
+            if (i,j) in nodes_visited:
+                continue
+            curr_len += 1
+            nodes_visited[(i,j)] = 1
+            curr_nodes.extend(graph[(i,j)])
+        if curr_len > longest_len:
+            longest_len, curr_len = curr_len, 0
+    return longest_len
 
 def create_graph(rows, cols, matrix):
     for row in matrix:
+        #append 0 element to beginning and end of list
+        row.insert(0,0)
         row.append(0)
-    new_row = [0] * cols
+    new_row = [0] * (cols+2)
+    matrix.append(new_row)
     graph = {}
+    nodes_tovisit = []
     for i in range(rows):
-        for j in range(cols):
+        for col_gr in range(1, cols+1):
+            j = col_gr-1
             if matrix[i][j] == 1:
+                nodes_tovisit.append((i,j))
+                if (i,j) not in graph:
+                    graph[(i,j)] = []
                 if matrix[i+1][j] == 1:
-                    if (i,j) in graph:
-                        graph[(i,j)].append((i+1,j))
-                    else:
-                        graph[(i,j)] = [(i+1,j)]
+                    graph[(i,j)].append((i+1,j))
                     if (i+1,j) in graph:
                         graph[(i+1,j)].append((i,j))
                     else:
                         graph[(i+1,j)] = [(i,j)]
                 if matrix[i][j+1] == 1:
-                    if (i,j) in graph:
-                        graph[(i,j)].append((i,j+1))
-                    else:
-                        graph[(i,j)] = [(i,j+1)]
+                    graph[(i,j)].append((i,j+1))
                     if (i,j+1) in graph:
                         graph[(i,j+1)].append((i,j))
                     else:
                         graph[(i,j+1)] = [(i,j)]
                 if matrix[i+1][j+1] == 1:
-                    if (i,j) in graph:
-                        graph[(i,j)].append((i+1,j+1))
-                    else:
-                        graph[(i,j)] = [(i+1,j+1)]
-                    if (i,j+1) in graph:
+                    graph[(i,j)].append((i+1,j+1))
+                    if (i+1,j+1) in graph:
                         graph[(i+1,j+1)].append((i,j))
                     else:
                         graph[(i+1,j+1)] = [(i,j)]
-    return graph            
+                if matrix[i-1][j-1] == 1:
+                    graph[(i,j)].append((i-1,j-1))
+                    if (i-1,j-1) in graph:
+                        graph[(i-1,j-1)].append((i,j))
+                    else:
+                        graph[(i-1,j-1)] = [(i,j)]
+    return graph, nodes_tovisit           
     
 def main():
     rows = int(input().strip())
@@ -50,7 +76,7 @@ def main():
     matrix = []
     for i in range(rows):
         matrix.append([int(i) for i in input().strip().split(' ')])
-    find_connections(rows, cols, matrix)
+    print(find_connections(rows, cols, matrix))
         
 if __name__ == '__main__':
     main()
