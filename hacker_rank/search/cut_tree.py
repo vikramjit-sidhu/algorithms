f33f2d89b21f20a1399b49166c79750d33231760
vikramjit-sidhu <vikram.sidhu.007@gmail.com>
2015-07-02 15:53:56 +0530

    doing a hacker rank q'n, cut trees, trying different approach

---
+++ hacker_rank/search/cut_tree.py
@@ -0,0 +1,67 @@
+"""
+Hacker Rank - Cut the tree
+https://www.hackerrank.com/challenges/cut-the-tree
+"""
+import pdb
+
+class TreeNode:
+    def __init__(self, val=0):
+       self.value = val
+       self.tot_value = None
+       self.parent = None
+       self.children = []
+
+
+def populate_total_height(node):
+    # pdb.set_trace()
+    if not node.children:
+        node.tot_value = node.value
+        return
+    val = node.value
+    for child in node.children:
+        if child.tot_value is None:
+            populate_total_height(child)
+        val += child.tot_value
+    node.tot_value = val
+
+def find_edge_remove(num_nodes, root):
+    populate_total_height(root)
+    min_diff = float('inf')
+    total_height = root.tot_value
+    max_child, max_subtr_val = None, 0
+    curr_node = root
+    while curr_node is not None:
+        if not curr_node.children:
+            break
+        for child in curr_node.children:
+            if child.tot_value > max_subtr_val:
+                max_child, max_subtr_val = child, child.tot_value
+            other_subtr = total_height - child.tot_value
+            diff = abs(child.tot_value - other_subtr)
+            if diff < min_diff:
+                min_diff = diff
+        curr_node = max_child
+        max_child, max_subtr_val = None, 0
+    return min_diff
+
+def main():
+    num_nodes = int(input().strip())
+    node_vals = [int(i) for i in input().strip().split(' ')]
+    nodes = []
+    for val in node_vals:
+        node = TreeNode(val)
+        nodes.append(node)
+    for i in range(num_nodes-1):
+        node1, node2 = (int(i) for i in input().strip().split(' '))
+        node1, node2 = node1-1, node2-1 #indexing it acc to how it is in nodes list
+        if nodes[node2].parent is not None:
+            nodes[node2].children.append(nodes[node1])
+            nodes[node1].parent = nodes[node2]
+        else:
+            nodes[node1].children.append(nodes[node2])
+            nodes[node2].parent = nodes[node1]
+    root = nodes[0]
+    print(find_edge_remove(num_nodes, root))
+
+if __name__ == '__main__':
+    main()
